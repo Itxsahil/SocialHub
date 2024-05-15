43066a5912304845796d2b2119103e4ecf09a971
itxsahil <sahilkh9087@gmail.com>
2024-05-15 12:55:09 +0530

    add get all tweets

--- src/controllers/Tweet.controller.js
+++ src/controllers/Tweet.controller.js
@@ -8,21 +8,54 @@ const createTweet = asyncHandler(async (req, res) => {
     const { content } = req.body
     const imageLocalPath = req.file?.path
     if (!content && !imageLocalPath) throw new ApiError(401, "Enter a valid msg")
-    let imageUrl;
+    let imageUrl = "";
     if (imageLocalPath) {
         imageUrl = await uploadOnCloudinary(imageLocalPath)
     }
-    
+
     const tweet = await Tweet.create({
         content,
         owner: req.user._id,
         image: imageUrl.url
     })
-    if(!tweet) throw new ApiError(400, "somthing went wrong while the operation")
+    if (!tweet) throw new ApiError(400, "somthing went wrong while the operation")
     return res.status(200)
-    .json(new ApiResponse(200, tweet, "Tweet is successfully done"))
+        .json(new ApiResponse(200, tweet, "Tweet is successfully done"))
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+    const { ownerId } = req.params
+    if (!ownerId) throw new ApiError(404, "provide a valid owner")
+    let page = parseInt(req.query.page) || 1
+    const pageSize = 10
+
+
+    let tweets;
+    let hasMore = true;
+    while (hasMore) {
+        // Fetch tweets for the current page
+        tweets = await Tweet.find({ owner: ownerId })
+            .sort({ createdAt: -1 })
+            .skip((page - 1) * pageSize)
+            .limit(pageSize);
+
+
+        // If no tweets found for this page, exit the loop
+        if (tweets.length === 0) break;
+
+        tweets.forEach(tweet => {
+            res.write(`data: ${JSON.stringify(tweet)}\n\n`);
+        });
+
+        page++; // Move to the next page
+        // Check if there are more tweets
+        hasMore = tweets.length === pageSize;
+    }
+    res.end();
 })
 
 export {
-    createTweet
+    createTweet,
+    getUserTweets
 }
--- src/routes/Tweet.routes.js
+++ src/routes/Tweet.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/Auth.middleware.js";
-import {createTweet} from "../controllers/Tweet.controller.js"
+import {createTweet,getUserTweets} from "../controllers/Tweet.controller.js"
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 router.route("/tweet").post(verifyJWT,upload.single("image"),createTweet)
+router.route("/:ownerId").get(getUserTweets)
 
 export default router;
