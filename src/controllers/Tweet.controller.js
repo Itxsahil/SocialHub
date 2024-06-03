ae4e47fb3c07339ad1bdf3f562e646b113ce0a53
itxsahil <sahilkh9087@gmail.com>
2024-06-03 11:52:40 +0530

    add update tweet, delete tweet

--- src/controllers/Tweet.controller.js
+++ src/controllers/Tweet.controller.js
@@ -2,6 +2,7 @@ import { Tweet } from "../model/tweet.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHendler.js"
+import { deleteFromeCloudinary } from "../utils/Cloudinary.delete.js"
 import { uploadOnCloudinary } from "../utils/Cloudinary.uploade.js"
 
 const createTweet = asyncHandler(async (req, res) => {
@@ -55,7 +56,94 @@ const getUserTweets = asyncHandler(async (req, res) => {
     res.end();
 })
 
+const updateTweet = asyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    const { content } = req.body;
+    const imageLocalPath = req.file?.path;
+
+    if (!content && !imageLocalPath) {
+        throw new ApiError(401, "Please enter valid content or image");
+    }
+
+    
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(401, "Unauthorized request");
+    }
+
+    let imageUrl = "";
+    if (imageLocalPath) {
+        const uploadResult = await uploadOnCloudinary(imageLocalPath);
+        if (uploadResult && uploadResult.url) {
+            imageUrl = uploadResult.url;
+        } else {
+            throw new ApiError(500, "Image upload failed");
+        }
+    }
+
+    // const tweet = await Tweet.findById(tweetId);
+    // if (!tweet) {
+    //     throw new ApiError(404, "Tweet not found");
+    // }
+
+    
+
+    const updateFields = {};
+    if (content) {
+        updateFields.content = content;
+    }
+    if (imageUrl) {
+        updateFields.image = imageUrl;
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        { $set: updateFields },
+        { new: true }
+    );
+
+    return res.status(200)
+        .json(new ApiResponse(200, 
+            updatedTweet, 
+            "Tweet updated successfully"
+        )
+    );
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+
+    const {tweetId} = req.params
+
+    if(!tweetId) throw new ApiError(401, "Pleas provide the tweetId")
+        
+    let tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(401, "Unauthorized request");
+    }
+    
+    tweet = await Tweet.findByIdAndDelete(tweetId)
+    await deleteFromeCloudinary(tweet.image)
+    return res.status(203)
+        .json(
+            new ApiResponse(204,
+                {}, 
+                "tweet deleted sucessfully"
+            )
+        )
+
+})
+
 export {
     createTweet,
-    getUserTweets
+    getUserTweets,
+    updateTweet,
+    deleteTweet
 }
--- src/routes/Tweet.routes.js
+++ src/routes/Tweet.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/Auth.middleware.js";
-import {createTweet,getUserTweets} from "../controllers/Tweet.controller.js"
+import {createTweet,getUserTweets,updateTweet,deleteTweet} from "../controllers/Tweet.controller.js"
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
 router.route("/tweet").post(verifyJWT,upload.single("image"),createTweet)
-router.route("/:ownerId").get(getUserTweets)
+router.route("/all/:ownerId").get(getUserTweets)
+router.route("/update-tweet/:tweetId").patch(verifyJWT,upload.single("image"),updateTweet)
+router.route("/delete-tweet/:tweetId").delete(verifyJWT,deleteTweet)
 
 export default router;
